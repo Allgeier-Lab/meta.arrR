b739f60231df22ba07a465b8a0d9a621e3c4821d
mhesselbarth <mhk.hesselbarth@gmail.com>
2021-12-21 09:39:16 -0500

    [skip-ci] Update result_readme

--- data-raw/result_readme.R
+++ data-raw/result_readme.R
@@ -9,7 +9,7 @@ starting_values <- meta.arrR::meta.arrR_starting_values
 parameters <- meta.arrR::meta.arrR_parameters
 
 # set number of metaecosystems
-n <- 5
+n <- 3
 
 # change values
 starting_values$pop_n <- rep(x = 5, n = n)
@@ -18,18 +18,18 @@ starting_values$pop_n <- rep(x = 5, n = n)
 reefs <- matrix(data = c(-1, 0, 0, 1, 1, 0, 0, -1, 0, 0), ncol = 2, byrow = TRUE)
 
 # setup extent and grain
-dimensions <- c(100, 100)
+dimensions <- c(50, 50)
 grain <- c(1, 1)
 
 # set time per iterations
 min_per_i <- 120
 
 # run the model for n years
-years <- 5
+years <- 3
 max_i <- (60 * 24 * 365 * years) / min_per_i
 
 # save results only every m days
-days <- 73 # which(max_i %% ((24 / (min_per_i / 60)) * (1:365)) == 0)
+days <- 15 # which(max_i %% ((24 / (min_per_i / 60)) * (1:365)) == 0)
 save_each <- (24 / (min_per_i / 60)) * days
 
 parameters$nutrients_output <- 0.1
@@ -44,7 +44,7 @@ starting_values$nutrients_pool <- stable_vals$nutrients_pool
 starting_values$detritus_pool <- stable_vals$detritus_pool
 
 nutr_input <- sim_nutr_input(n = n, max_i = max_i, input_mn = stable_vals$nutr_input,
-                             freq_mn = 5, variability = 0.1)
+                             freq_mn = 5, variability = 0.5)
 
 metasyst <- setup_meta(n = n, max_i = max_i, dimensions = dimensions, grain = grain,
                        reefs = reefs, starting_values = starting_values, parameters = parameters)
